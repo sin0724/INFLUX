7ec2a69f72c5eb5fe3b73e9742cecdea7cb4884d
sin0724 <hyuun0724@gmail.com>
2026-01-08 11:53:56 +0900

    Fix build error: remove extra blank line in ReviewOrdersManagement

--- components/ReviewOrdersManagement.tsx
+++ components/ReviewOrdersManagement.tsx
@@ -556,7 +556,6 @@ export default function ReviewOrdersManagement() {
     return filtered;
   }, [orders, filters.status]);
 
-
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 py-6">
